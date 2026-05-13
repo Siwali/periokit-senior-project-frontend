cbe1db2051afe1bc24d30e9c2509ba4d1c5abe23
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2026-05-13 17:15:01 +0700

    chore(deps): add vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue(),
     tailwindcss(),
   ],
