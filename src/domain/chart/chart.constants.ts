d75a13083638223626fc77eb243e3a1c1a8722d3
Siwali <sambuk744@gmail.com>
2026-05-16 21:37:24 +0700

    refactor(chart): rename inner surface rows
    - Rename PALATAL_ROWS to INNER_SURFACE_ROWS so row labels apply to both palatal and lingual chart surfaces.

--- src/domain/chart/chart.constants.ts
+++ src/domain/chart/chart.constants.ts
@@ -17,7 +17,7 @@ export const LOWER_ARCH = [
 ]
 
 export const BUCCAL_ROWS = ['Implant', 'Mobility', 'Keratinized', 'Furcation', 'BOP', 'PI', 'Recession', 'PD', 'CAL']
-export const PALATAL_ROWS = ['CAL', 'PD', 'Recession', 'PI', 'BOP', 'Furcation', 'Keratinized', 'Mobility', 'Implant']
+export const INNER_SURFACE_ROWS = ['CAL', 'PD', 'Recession', 'PI', 'BOP', 'Furcation', 'Keratinized', 'Mobility', 'Implant']
 export const SITE_INDEXES: SiteIndex[] = [0, 1, 2]
 
 export const CHART_LEGEND_ITEMS = {
